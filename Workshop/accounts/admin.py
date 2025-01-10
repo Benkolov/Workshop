968247a47704eb2851be8d91dbe8d3a057032b91
Benkolov <111705203+Benkolov@users.noreply.github.com>
2025-01-10 10:16:11 +0200

    Add

--- Workshop/accounts/admin.py
+++ Workshop/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin, get_user_model
 
-# Register your models here.
+
+@admin.register(get_user_model())
+class AppUserAdmin(auth_admin.UserAdmin):
+    pass
+
