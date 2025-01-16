c49bab47f337fd0135c1d0c9b3b2f31eb1e6ae9c
Benkolov <111705203+Benkolov@users.noreply.github.com>
2025-01-16 19:50:42 +0200

    Changes

--- Workshop/settings.py
+++ Workshop/settings.py
@@ -70,11 +70,11 @@ WSGI_APPLICATION = 'Workshop.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": 'django.db.backends.postgresql',
-        "NAME": os.getenv("POSTGRES_DB", "workshop_db"),
-        "USER": os.getenv("POSTGRES_USER", "postgres-user"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "password"),
-        "HOST": os.getenv("DB_HOST", "db"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("DB_HOST",),
+        "PORT": os.getenv("DB_PORT",),
     }
 }
 
